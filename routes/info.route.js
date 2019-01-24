12ed9e5a3532c8d909dc89eae435c8669bc592fc
Ashish Bagaddeo <ashish@bagaddeo.com>
2019-01-24 04:30:24 +0000

    Dashboad info and stock updation

--- routes/info.route.js
+++ routes/info.route.js
@@ -4,11 +4,36 @@ const { Customer } = require('../models/customer.model');
 const { Product } = require('../models/product.model');
 const router = express.Router();
 
-router.get('/orderstatus', async (req, res) => {
+router.get('/orderinfo', async (req, res) => {
 
+    let matchCond = {};
+
+    matchCond['year'] = {$eq : +req.query.year || new Date().getFullYear()};
+
+    if (req.query.month) {
+        matchCond['month'] = {$eq : +req.query.month};
+    } 
+    else {
+        if (req.query.startmonth) {
+            matchCond['$and'] = 
+            [{'month': {'$gte': +req.query.startmonth}}, 
+             {'month': {'$lte' : +req.query.endmonth || +req.query.startmonth}}];
+        }
+    }
+    
     // Group By Status and then Count, sum up revenue and total tax
     const status = await Order
     .aggregate([
+        { $project: {
+            _id: '$_id',
+            status: '$status',
+            finalamount: '$finalamount',
+            totaltax: '$totaltax',
+            year: { $year : '$date'},
+            month: { $month: '$date' }
+        }},
+        {$match: matchCond
+        },
         {$group: {
             _id: '$status',
             count: {$sum: 1},
@@ -40,6 +65,18 @@ router.get('/products', async (req, res) => {
     const count = await Product.estimatedDocumentCount();
     if (!count) return res.status(404).send('Not Found');
     res.status(200).json({"count":count});
+});
+
+router.get('/taxforyear', async (req, res) => {
+
+    let curYear = req.params.year || new Date().getFullYear();
+
+    const monthTax = Order.aggregate([
+        {
+            $match: {} 
+        }
+    ]);
+
 });
 
 module.exports = router;
