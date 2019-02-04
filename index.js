ca1bfff92c473712e673228935e0373d5a697a13
Ashish Bagaddeo <ashish@bagaddeo.com>
2019-02-04 19:26:58 +0000

    Almost done

--- index.js
+++ index.js
@@ -25,6 +25,52 @@ process.on("unhandledRejection", (error) => {
 
 connectDB();
 
+
+const normalizePort = val => {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  };
+  
+  const onError = error => {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+  
+  const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    debug("Listening on " + bind);
+  };
+  
+  const port = normalizePort(process.env.PORT || "3000");
+  app.set("port", port);
+  
+
 // Express Config Settings
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -52,8 +98,6 @@ app.use((req, res) => {
 });
 
 app.use(errorHandler);
-
-const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 server.on("error", onError);
